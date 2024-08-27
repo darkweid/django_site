8eb8203c5f573f2bba4e0b71ed838d69d7950451
Mark Shamanskii <darkweid713@gmail.com>
2024-08-27 21:33:36 +0500

    Add multiple pages

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, re_path, register_converter
 from . import views
 
-
-
 urlpatterns = [
     path('', views.index, name='home'),  # http://127.0.0.1:8000
     path('explore-topics/', views.explore_topics, name='explore-topics'),
@@ -10,9 +8,8 @@ urlpatterns = [
     path('reviews/', views.reviews, name='reviews'),
     path('questionnaire/', views.questionnaire, name='questionnaire'),
     path('login/', views.login, name='login'),
-    path('submit_form/', views.submit_form, name='submit_form'),
-    path('success/', views.success_page, name='success_page'),  # Страница успешной отправки
+    path('survey-thank-you/', views.survey_thank_you, name='survey_thank_you'),  # Страница успешной отправки
+    path('admin/survey/<int:survey_id>/responses/', views.survey_responses_view, name='admin_survey_responses'),
+    path('submit-request/', views.submit_request, name='submit_request'),
 
-    # path('cats/<int:cat_id>/', views.categories, name='cats_id'), # http://127.0.0.1:8000/cats/2/
-    # path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),  # http://127.0.0.1:8000/cats/fdgdg/
 ]
