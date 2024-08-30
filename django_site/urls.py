684d6b6e701c7e3dc8afe277cd6c16446213fbb6
Mark Shamanskii <darkweid713@gmail.com>
2024-08-30 20:20:41 +0500

    Add Users app to urlpatterns

--- django_site/urls.py
+++ django_site/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from myapp import views
 
-
 urlpatterns = [
     path('', include('myapp.urls')),
+    path('users/', include('users.urls', namespace='users')),
+
     path('admin/', admin.site.urls),
 ]
 
