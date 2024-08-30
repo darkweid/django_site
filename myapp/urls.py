53d6b33d954db29f01bd4af2359919067de81e91
Mark Shamanskii <darkweid713@gmail.com>
2024-08-30 20:21:13 +0500

    Rename page login —> lk

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, re_path, register_converter
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),  # http://127.0.0.1:8000
+    path('', views.index, name='home'),
     path('explore-topics/', views.explore_topics, name='explore-topics'),
     path('contact/', views.contact, name='contact'),
     path('reviews/', views.reviews, name='reviews'),
     path('questionnaire/', views.questionnaire, name='questionnaire'),
-    path('login/', views.login, name='login'),
+    path('lk/', views.lk, name='lk'),
     path('survey-thank-you/', views.survey_thank_you, name='survey_thank_you'),  # Страница успешной отправки
     path('admin/survey/<int:survey_id>/responses/', views.survey_responses_view, name='admin_survey_responses'),
     path('submit-request/', views.submit_request, name='submit_request'),
