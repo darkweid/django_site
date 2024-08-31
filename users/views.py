efdab8c93b85ed2fdfc505eb4310574659202c8b
Mark Shamanskii <darkweid713@gmail.com>
2024-08-31 17:25:32 +0500

    Add classes for registration, login, and logout

--- users/views.py
+++ users/views.py
@@ -1,3 +1,51 @@
+from django.contrib import messages
+from django.contrib.auth import logout, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, UpdateView
 
-# Create your views here.
+from users.forms import LoginUserForm, RegisterUserForm, ProfileUserForm
+
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+    extra_context = {'title': 'Авторизация'}
+
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {'title': 'Регистрация'}
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Вы успешно зарегистрировались!<br>Теперь вы можете войти в свой профиль.')
+        return response
+
+
+class ProfileUser(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = ProfileUserForm
+    template_name = 'users/profile.html'
+    extra_context = {'title': 'Мой профиль'}
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Данные профиля изменены')
+        return response
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('users:login'))
