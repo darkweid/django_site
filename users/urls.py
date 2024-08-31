adf6d900cf66e687c27867e01be8d31d586f3153
Mark Shamanskii <darkweid713@gmail.com>
2024-08-31 17:23:21 +0500

    Add urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
-    path('login/', views.login_user, name='login'),
+    path('login/', views.LoginUser.as_view(), name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.RegisterUser.as_view(), name='register'),
+    path('profile/', views.ProfileUser.as_view(), name='profile'),
 
 ]
