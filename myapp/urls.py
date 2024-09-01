24de24510e51b86508c08edb7977c4f198dca5aa
Mark Shamanskii <darkweid713@gmail.com>
2024-09-01 13:50:17 +0500

    Add urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,9 +7,14 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('reviews/', views.reviews, name='reviews'),
     path('questionnaire/', views.questionnaire, name='questionnaire'),
-    path('lk/', views.lk, name='lk'),
     path('survey-thank-you/', views.survey_thank_you, name='survey_thank_you'),  # Страница успешной отправки
     path('admin/survey/<int:survey_id>/responses/', views.survey_responses_view, name='admin_survey_responses'),
     path('submit-request/', views.submit_request, name='submit_request'),
+    path('lk/', views.lk, name='lk'),
+    path('lk/grammar/', views.grammar_sections, name='grammar_sections'),
+    path('lk/grammar/<int:section_id>/', views.grammar_materials, name='grammar_materials'),
+    path('lk/homework/', views.homework_list, name='homework_list'),
+    path('lk/homework/<int:homework_id>/submit/', views.submit_homework, name='submit_homework'),
+    path('lk/completed-homework/', views.completed_homework, name='completed_homework'),
 
 ]
