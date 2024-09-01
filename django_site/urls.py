308eb57f34d4104e85b9849490896903a3aa66c3
Mark Shamanskii <darkweid713@gmail.com>
2024-09-01 13:50:10 +0500

    Add media loading in Debug mode

--- django_site/urls.py
+++ django_site/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from django_site import settings
 from myapp import views
 
 urlpatterns = [
     path('', include('myapp.urls')),
     path('users/', include('users.urls', namespace='users')),
-
     path('admin/', admin.site.urls),
 ]
 
@@ -29,4 +31,5 @@ handler404 = views.page_not_found
 admin.site.site_header = 'Панель администрирования'
 admin.site.index_title = ''
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
